cc4d4dddc32421e7ca64eb7ac5f065228835b6d2
Damilola Balogun <balogundamilola.f@gmail.com>
2023-06-18 23:47:01 +0100

    OR Assignment Variable

--- javascriptcomplete2023/09-Data-Structures-Operators/starter/script.js
+++ javascriptcomplete2023/09-Data-Structures-Operators/starter/script.js
@@ -282,3 +282,25 @@ const guestCorrect = restaurant.numGuests ?? 10
 console.log(guestCorrect)
 //it deals with null, empty amd undefined values
 
+
+
+const rest1 = {
+  name : 'Capri',
+  numGuests : 28
+}
+
+const rest2 = {
+  name : 'La Piazza',
+  owner : 'Giovanni Rossi'
+}
+
+rest2.numGuests = rest2.numGuests || 10
+// rest1.numGuests = rest1.numGuests || 10
+
+// console.log(rest2.numGuests, rest1.numGuests)
+
+
+//OR ASSIGNMENT VARIABLE
+rest1.numGuests ||= 10
+rest2.numGuests ||= 10
+console.log(rest2.numGuests, rest1.numGuests)
